57964d853161f86177a4a8b7aa7586a72c4b89a2
Christopher Guckes <christopher.guckes@torq-dev.de>
2016-10-16 13:33:13 +0200

    Added BankAccount class and test

---
+++ src/main/java/com/alphatrader/rest/BankAccount.java
@@ -0,0 +1,123 @@
+package com.alphatrader.rest;
+
+import com.alphatrader.rest.util.Http;
+import com.google.gson.Gson;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * Represents a bank account in the game.
+ *
+ * @author Christopher Guckes (christopher.guckes@torq-dev.de)
+ * @version 1.0.0
+ */
+public class BankAccount {
+    /**
+     * The logger for this class.
+     */
+    private static final Log log = LogFactory.getLog(BankAccount.class);
+
+    /**
+     * The gson wrapper for this class.
+     */
+    private static final Gson gson = new Gson();
+
+    /**
+     * The amount of cash stored in this bank account.
+     */
+    private final double cash;
+
+    /**
+     * The unique id of this bank account.
+     */
+    private final String id;
+
+    /**
+     * Creates a new bank account with the given parameters.
+     *
+     * @param id the unique id of this account
+     * @param cash the amount of cash in this account
+     */
+    public BankAccount(String id, double cash) {
+        this.id = id;
+        this.cash = cash;
+    }
+
+    /**
+     * @return the bank account of the logged in user
+     */
+    public static BankAccount getUserBankAccount() {
+        BankAccount myReturn = null;
+        try {
+            HttpResponse<JsonNode> response = Http.getInstance().get("/api/bankaccounts");
+            myReturn = gson.fromJson(
+                response.getBody()
+                    .getObject()
+                    .toString(), BankAccount.class);
+        }
+        catch (UnirestException ue) {
+            log.error("Error loading bank account!");
+            StringWriter stringWriter = new StringWriter();
+            ue.printStackTrace(new PrintWriter(stringWriter));
+            log.debug(stringWriter.toString());
+        }
+
+        return myReturn;
+    }
+
+    /**
+     * @return the amount of cash in this bank account
+     */
+    public double getCash() {
+        return cash;
+    }
+
+    /**
+     * @return the id of this bank account
+     */
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{"
+            + "cash=" + cash
+            + ", id='" + id + '\''
+            + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        BankAccount that = (BankAccount) o;
+
+        if (Double.compare(that.cash, cash) != 0) {
+            return false;
+        }
+        return id != null ? id.equals(that.id) : that.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(cash);
+        result = (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/test/java/com/alphatrader/rest/BankAccountTest.java
@@ -0,0 +1,67 @@
+package com.alphatrader.rest;
+
+import com.alphatrader.rest.util.Http;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import org.apache.http.HttpResponseFactory;
+import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.DefaultHttpResponseFactory;
+import org.apache.http.message.BasicStatusLine;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test case for the {@link BankAccount} class.
+ *
+ * @author Christopher Guckes (christopher.guckes@torq-dev.de)
+ * @version 1.0.0
+ */
+public class BankAccountTest {
+    private static final String JSON = "{\n" +
+        "  \"cash\": 10000,\n" +
+        "  \"id\": \"443db0b7-c399-4252-8166-061d8de1d634\"\n" +
+        "}";
+
+    private BankAccount toTest;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        HttpResponseFactory factory = new DefaultHttpResponseFactory();
+        org.apache.http.HttpResponse response = factory.newHttpResponse(new BasicStatusLine(HttpVersion.HTTP_1_1, HttpStatus.SC_OK, null), null);
+        response.setEntity(new StringEntity(JSON));
+        HttpResponse<JsonNode> httpResponse = new HttpResponse<>(response, JsonNode.class);
+
+        Http mockHttp = mock(Http.class);
+        when(mockHttp.get(any(String.class))).thenReturn(httpResponse);
+        Http.setInstance(mockHttp);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        toTest = new BankAccount("443db0b7-c399-4252-8166-061d8de1d634", 10000.0);
+    }
+
+    @Test
+    public void testGetUserBankAccount() throws Exception {
+        BankAccount testAccount = BankAccount.getUserBankAccount();
+        assertEquals(toTest, testAccount);
+    }
+
+    @Test
+    public void testGetCash() throws Exception {
+        assertEquals(10000.0, toTest.getCash(), 0.0001);
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+        assertEquals("443db0b7-c399-4252-8166-061d8de1d634", toTest.getId());
+    }
+}
