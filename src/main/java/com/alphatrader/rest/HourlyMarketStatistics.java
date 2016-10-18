a920b388d99cd6874fe66976ec252ab92fdd65ae
Christopher Guckes <christopher.guckes@torq-dev.de>
2016-10-18 11:49:41 +0200

    Finalized the hourly market statistics class.

--- src/main/java/com/alphatrader/rest/HourlyMarketStatistics.java
+++ src/main/java/com/alphatrader/rest/HourlyMarketStatistics.java
@@ -43,42 +43,189 @@ public class HourlyMarketStatistics {
         new TypeToken<ArrayList<HourlyMarketStatistics>>() {
         }.getType();
 
+    /**
+     * The unique id of the statistics object.
+     */
     private final String id = null;
+
+    /**
+     * The average bond duration in days.
+     */
     private final Double averageBondDurationInDays = null;
+
+    /**
+     * The average book value.
+     */
     private final Double averageBookValue = null;
+
+    /**
+     * The average daily wage.
+     */
     private final Double averageDailyWage = null;
+
+    /**
+     * The average designated sponsor rating.
+     */
     private final Double averageDesignatedSponsorRating = null;
+
+    /**
+     * The average yield to maturity.
+     */
     private final Double averageYieldToMaturity = null;
+
+    /**
+     * The cumulative face volume of all bonds.
+     */
     private final Double bondFaceVolume = null;
+
+    /**
+     * The cumulative central bank reserves.
+     */
     private final Double centralBankReserves = null;
+
+    /**
+     * The cumulative committed cash.
+     */
     private final Double committedCash = null;
+
+    /**
+     * The cumulative corporate cash.
+     */
     private final Double corporateCash = null;
+
+    /**
+     * The date the statistics was created.
+     */
     private final LocalDateTime date = null;
+
+    /**
+     * The current main interest rate.
+     */
     private final Double mainInterestRate = null;
+
+    /**
+     * The current market cap.
+     */
     private final Double marketCap = null;
+
+    /**
+     * The number of active listings.
+     */
     private final Integer numberOfActiveListings = null;
+
+    /**
+     * The number of banks.
+     */
     private final Integer numberOfBanks = null;
+
+    /**
+     * The number of bond orders.
+     */
     private final Integer numberOfBondOrders = null;
+
+    /**
+     * The number of cashout polls.
+     */
     private final Integer numberOfCashoutPolls = null;
+
+    /**
+     * The number of committed shares.
+     */
     private final Integer numberOfCommittedShares = null;
+
+    /**
+     * The number of companies.
+     */
     private final Integer numberOfCompanies = null;
+
+    /**
+     * The number of designated sponsors.
+     */
     private final Integer numberOfDesignatedSponsors = null;
+
+    /**
+     * The number of liquidation polls.
+     */
     private final Integer numberOfLiquidationPolls = null;
+
+    /**
+     * The number of orders.
+     */
     private final Integer numberOfOrders = null;
+
+    /**
+     * The number of orders in the last 24 hours.
+     */
     private final Integer numberOfOrders24h = null;
+
+    /**
+     * The number of over the counter orders.
+     */
     private final Integer numberOfOtcOrders = null;
+
+    /**
+     * The number of other listings.
+     */
     private final Integer numberOfOtherListings = null;
+
+    /**
+     * The number of other orders.
+     */
     private final Integer numberOfOtherOrders = null;
+
+    /**
+     * The number of users with partner status.
+     */
     private final Integer numberOfPartnerUsers = null;
+
+    /**
+     * The number of users with premium status.
+     */
     private final Integer numberOfPremiumUsers = null;
+
+    /**
+     * The number of repo orders.
+     */
     private final Integer numberOfRepoOrders = null;
+
+    /**
+     * The number of stock orders.
+     */
     private final Integer numberOfStockOrders = null;
+
+    /**
+     * The number of system bond orders.
+     */
     private final Integer numberOfSystemBondOrders = null;
+
+    /**
+     * The number of system repo orders.
+     */
     private final Integer numberOfSystemRepoOrders = null;
+
+    /**
+     * The number of users.
+     */
     private final Integer numberOfUsers = null;
+
+    /**
+     * The order volume.
+     */
     private final Double orderVolume = null;
+
+    /**
+     * The order volume in the last 24 hours.
+     */
     private final Double orderVolume24h = null;
+
+    /**
+     * The cumulative amount of private cash.
+     */
     private final Double privateCash = null;
+
+    /**
+     * The cumulative amount of system bond face volumes.
+     */
     private final Double systemBondFaceVolume = null;
 
     /**
@@ -109,150 +256,261 @@ public class HourlyMarketStatistics {
         return myReturn;
     }
 
+    /**
+     * @return the unique id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * @return the average bond duration in days
+     */
     public Double getAverageBondDurationInDays() {
         return averageBondDurationInDays;
     }
 
+    /**
+     * @return the average book value
+     */
     public Double getAverageBookValue() {
         return averageBookValue;
     }
 
+    /**
+     * @return the average daily wage
+     */
     public Double getAverageDailyWage() {
         return averageDailyWage;
     }
 
+    /**
+     * @return the average designated sponsor rating
+     */
     public Double getAverageDesignatedSponsorRating() {
         return averageDesignatedSponsorRating;
     }
 
+    /**
+     * @return the average yield to maturity
+     */
     public Double getAverageYieldToMaturity() {
         return averageYieldToMaturity;
     }
 
+    /**
+     * @return the cumulative bond face volume
+     */
     public Double getBondFaceVolume() {
         return bondFaceVolume;
     }
 
+    /**
+     * @return the amount of central bank reserves
+     */
     public Double getCentralBankReserves() {
         return centralBankReserves;
     }
 
+    /**
+     * @return the amount of committed cash
+     */
     public Double getCommittedCash() {
         return committedCash;
     }
 
+    /**
+     * @return the amount of corporate cash
+     */
     public Double getCorporateCash() {
         return corporateCash;
     }
 
+    /**
+     * @return the date
+     */
     public LocalDateTime getDate() {
         return date;
     }
 
+    /**
+     * @return the main interest rate
+     */
     public Double getMainInterestRate() {
         return mainInterestRate;
     }
 
+    /**
+     * @return the market cap
+     */
     public Double getMarketCap() {
         return marketCap;
     }
 
+    /**
+     * @return the number of active listings
+     */
     public Integer getNumberOfActiveListings() {
         return numberOfActiveListings;
     }
 
+    /**
+     * @return the number of banks
+     */
     public Integer getNumberOfBanks() {
         return numberOfBanks;
     }
 
+    /**
+     * @return the number of bond orders
+     */
     public Integer getNumberOfBondOrders() {
         return numberOfBondOrders;
     }
 
+    /**
+     * @return the number of cashout polls
+     */
     public Integer getNumberOfCashoutPolls() {
         return numberOfCashoutPolls;
     }
 
+    /**
+     * @return the number of committed shares
+     */
     public Integer getNumberOfCommittedShares() {
         return numberOfCommittedShares;
     }
 
+    /**
+     * @return the number of companies
+     */
     public Integer getNumberOfCompanies() {
         return numberOfCompanies;
     }
 
+    /**
+     * @return the number of designated sponsors
+     */
     public Integer getNumberOfDesignatedSponsors() {
         return numberOfDesignatedSponsors;
     }
 
+    /**
+     * @return the number of liquidation polls
+     */
     public Integer getNumberOfLiquidationPolls() {
         return numberOfLiquidationPolls;
     }
 
+    /**
+     * @return the number of orders
+     */
     public Integer getNumberOfOrders() {
         return numberOfOrders;
     }
 
+    /**
+     * @return the number of orders in the last 24 hours
+     */
     public Integer getNumberOfOrders24h() {
         return numberOfOrders24h;
     }
 
+    /**
+     * @return the number of over the counter orders
+     */
     public Integer getNumberOfOtcOrders() {
         return numberOfOtcOrders;
     }
 
+    /**
+     * @return the number of other listings
+     */
     public Integer getNumberOfOtherListings() {
         return numberOfOtherListings;
     }
 
+    /**
+     * @return the number of other orders
+     */
     public Integer getNumberOfOtherOrders() {
         return numberOfOtherOrders;
     }
 
+    /**
+     * @return the number of users with partner status
+     */
     public Integer getNumberOfPartnerUsers() {
         return numberOfPartnerUsers;
     }
 
+    /**
+     * @return the number of users with premium status
+     */
     public Integer getNumberOfPremiumUsers() {
         return numberOfPremiumUsers;
     }
 
+    /**
+     * @return the number of repo orders
+     */
     public Integer getNumberOfRepoOrders() {
         return numberOfRepoOrders;
     }
 
+    /**
+     * @return the number of stock orders
+     */
     public Integer getNumberOfStockOrders() {
         return numberOfStockOrders;
     }
 
+    /**
+     * @return the number of system bond orders
+     */
     public Integer getNumberOfSystemBondOrders() {
         return numberOfSystemBondOrders;
     }
 
+    /**
+     * @return the number of system repo orders
+     */
     public Integer getNumberOfSystemRepoOrders() {
         return numberOfSystemRepoOrders;
     }
 
+    /**
+     * @return the number of users
+     */
     public Integer getNumberOfUsers() {
         return numberOfUsers;
     }
 
+    /**
+     * @return the cumulative order volume
+     */
     public Double getOrderVolume() {
         return orderVolume;
     }
 
+    /**
+     * @return the cumulative order volume in the last 24 hours
+     */
     public Double getOrderVolume24h() {
         return orderVolume24h;
     }
 
+    /**
+     * @return the cumulative private cash
+     */
     public Double getPrivateCash() {
         return privateCash;
     }
 
+    /**
+     * @return the cumulative system bond face volume
+     */
     public Double getSystemBondFaceVolume() {
         return systemBondFaceVolume;
     }
